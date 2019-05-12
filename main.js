6daeff815c29871f4ddd24d25264caf16eb04ec7
Nagindra <nagi@Nagindras-MacBook-Air.local>
2019-05-12 17:18:36 +0200

    second commit for yelp (business details)

--- main.js
+++ main.js
@@ -8,16 +8,37 @@ if (window.localStorage.getItem('categories') === null) {
     categories = window.localStorage.getItem('categories');
     createMainCategories(JSON.parse(categories));
 }
-
+// static list group
 document.querySelector('#listGroup').addEventListener('click', function (event) {
     const category = event.target.getAttribute('category');
     getBusinesses(category);
 });
+// more categories click event
+document.querySelector('#listGroup a:last-child')
+    .addEventListener('click', function (event) {
+        var listGroup = document.querySelector('#listGroup');
+        var listGroupMore = document.querySelector('#listGroup_more');
+        listGroup.style.display = 'none';
+        listGroupMore.style.display = 'flex';
+    });
 
 document.querySelector('#listGroup_more').addEventListener('click', function (event) {
     const category = event.target.getAttribute('category');
     getBusinesses(category);
+
 });
+
+//do something when switch is clicked
+document.querySelector('.switch').addEventListener('click', function (event) {
+    console.log(event.target.value);
+});
+
+//To get the details of the business
+document.querySelector('#business_list').addEventListener('click', function (event) {
+    const businessId = event.target.parentElement.getAttribute('businessId');
+    createBusinessListById(businessId);
+});
+
 
 function getCategories() {
     const categories_url = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/categories';
@@ -56,22 +77,14 @@ function createMainCategoriesList(categories) {
     let listGroupHTML = document.querySelector('#listGroup_more').innerHTML;
     let anchorElems = '';
     categories.forEach(function (element) {
-        let anchorElem = '<a class="list-group-item list-group-item-action icon_separator" category="' + element.title + '"><span category="' + element.title + '">' +
-            element.title + '</span><i class="fas fa-chevron-right"></i></a>';
+        let anchorElem = '<a class="list-group-item list-group-item-action icon_separator"' + 'category=' + element.title + '>' +
+            element.title + '<i class="fas fa-chevron-right"></i></a>';
         anchorElems = anchorElems + anchorElem;
     });
 
     listGroupHTML = listGroupHTML + anchorElems;
     document.querySelector('#listGroup_more').innerHTML = listGroupHTML;
 }
-
-document.querySelector('#listGroup a:last-child')
-    .addEventListener('click', function (event) {
-        var listGroup = document.querySelector('#listGroup');
-        var listGroupMore = document.querySelector('#listGroup_more');
-        listGroup.style.display = 'none';
-        listGroupMore.style.display = 'flex';
-    });
 
 let businesses = [];
 
@@ -95,9 +108,11 @@ function getBusinesses(category) {
     }
 }
 
+
 function createBusinessList(businesses) {
     document.querySelector('#listGroup').style.display = 'none';
     document.querySelector('#listGroup_more').style.display = 'none';
+    document.querySelector('.switch').style.display = 'flex';
 
     let businessListHTML = document.querySelector('#business_list').innerHTML;
     let mainCard = '';
@@ -108,12 +123,12 @@ function createBusinessList(businesses) {
         if (business.price) {
             price = business.price;
         }
-        let card = '<div class="card">' +
+        let card = '<div class="card" businessId ="' + business.id + '">' +
             '<img class="card-img-left img-thumbnail" src="' + business.image_url + '" alt="Card image cap">' +
             '<div class="card-body"><span class="title_distance"><h5 class="card-title">' + business.name + '</h5><h6>' + distance + ' km</h6></span>' +
             '<h6 class="card-text">' + business.review_count +
-            ' posts</h6><div><h6>' + price + ' ' + location.country +
-            '</h6></div><div><h6>' + location.address1 + ' ' + location.city + '</h6></div></div></div>';
+            ' posts</h6><div businessId ="' + business.id + '"><h6>' + price + ' ' + location.country +
+            '</h6></div><div businessId ="' + business.id + '"><h6>' + location.address1 + ' ' + location.city + '</h6></div></div></div>';
 
         mainCard = mainCard + card;
     });
@@ -121,3 +136,61 @@ function createBusinessList(businesses) {
     businessListHTML = businessListHTML + mainCard;
     document.querySelector('#business_list').innerHTML = businessListHTML;
 }
+
+function createBusinessListById(businessId) {
+    if (businessId !== null) {
+        const businessDetailsByIDUrl = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/' + businessId
+        fetch(businessDetailsByIDUrl, {
+            method: 'GET', // or 'PUT' // data can be `string` or {object}!
+            headers: {
+                'Authorization': authorizationKey,
+                'Access-Control-Allow-Origin': '*'
+            }
+        }).then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            console.log(data);
+            createBusinessDetails(data);
+        });
+    }
+}
+
+function createBusinessDetails(business) {
+    document.querySelector('#listGroup').style.display = 'none';
+    document.querySelector('#listGroup_more').style.display = 'none';
+    document.querySelector('.switch').style.display = 'none';
+    document.querySelector('#business_list').style.display = 'none';
+
+    let businessDetailsHtml = document.querySelector('#business_details').innerHTML;
+    //console.log(business.name);
+    let photosDiv = '';
+    business.photos.forEach(function (photo) {
+        let photoDiv = '<img src="' + photo + '" class="card-img-bottom img-thumbnail"/>';
+        photosDiv = photosDiv + photoDiv;
+    });
+
+    let name = '<div class="textsize"><h5 class="card-title ">' + business.name + '</h5><div>' + business.categories[0].title + '</div><div><span>' + business.hours[0].open[0].start + ' - ' + business.hours[0].open[0].end + '</span></div><div><span>' + 'contribution' + '</span>' + 'call' + '<span>' + 'Direction' + ' </span>' + 'website' + '<span></span><span></span></div><div>' + photosDiv + '</div></div>';
+
+    document.querySelector('#business_details').innerHTML = name;
+}
+
+// function getStars(rating) {
+
+//   // Round to nearest half
+//   rating = Math.round(rating * 2) / 2;
+//   let output = [];
+
+//   // Append all the filled whole stars
+//   for (var i = rating; i >= 1; i--)
+//     output.push('<i class="fa fa-star" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+
+//   // If there is a half a star, append it
+//   if (i == .5) output.push('<i class="fa fa-star-half-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+
+//   // Fill the empty stars
+//   for (let i = (5 - rating); i >= 1; i--)
+//     output.push('<i class="fa fa-star-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+
+//   return output.join('');
+
+// }
